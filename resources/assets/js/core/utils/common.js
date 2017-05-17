ebc34ca1c3c0ce981206dda20fd86f47f6f98d78
刚说 <samuel_abe@163.com>
2017-05-17 11:37:42 +0800

    Update common.js

--- resources/assets/js/core/utils/common.js
+++ resources/assets/js/core/utils/common.js
@@ -1,26 +1,16 @@
-const isArray = (value) => {
-    return value && value.constructor === Array;
-};
 
-function isString(val) {
-    return typeof val === 'string';
-}
-
-function isNumber(val) {
-    return typeof val === 'number';
-}
-
-function isUndefined(val) {
-    return typeof val === 'undefined';
-}
-
-function isObject(val) {
-    return val !== null && typeof val === 'object';
-}
-
-function isDate(val) {
-    return toString.call(val) === '[object Date]';
-}
+const isArray=_.isArray;
+const bind=_.bind;
+const spread=_.spread;
+const merge=_.merge;
+const extend=_.extend;
+const forEach=_.forEach;
+const isString=_.isString;
+const isNumber=_.isNumber;
+const isUndefined=_.isUndefined;
+const isObject=_.isObject;
+const isDate=_.isDate;
+const debounce =_.debounce;
 
 function isFile(val) {
     return toString.call(val) === '[object File]';
@@ -42,43 +32,10 @@ function trim(str) {
     return str.replace(/^\s*/, '').replace(/\s*$/, '');
 }
 
-function bind(fn, thisArg) {
-    return function wrap() {
-        var args = new Array(arguments.length);
-        for (var i = 0; i < args.length; i++) {
-            args[i] = arguments[i];
-        }
-        return fn.apply(thisArg, args);
-    };
-};
 function combineURLs(baseURL, relativeURL) {
     return relativeURL ? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '') : baseURL;
 };
-/**
- * Syntactic sugar for invoking a function and expanding an array for arguments.
- *
- * Common use case would be to use `Function.prototype.apply`.
- *
- *  ```js
- *  function f(x, y, z) {}
- *  var args = [1, 2, 3];
- *  f.apply(null, args);
- *  ```
- *
- * With `spread` this example can be re-written.
- *
- *  ```js
- *  spread(function(x, y, z) {})([1, 2, 3]);
- *  ```
- *
- * @param {Function} callback
- * @returns {Function}
- */
-function spread(callback) {
-  return function wrap(arr) {
-    return callback.apply(null, arr);
-  };
-};
+
 function isAbsoluteURL(url) {
     // A URL is considered absolute if it begins with "<scheme>://" or "//" (protocol-relative URL).
     // RFC 3986 defines scheme name as a sequence of characters beginning with a letter and followed
@@ -88,20 +45,7 @@ function isAbsoluteURL(url) {
 const uniqueId = () => {
     return Math.random().toString(36).slice(4);
 };
-const debounce = (callback, limit) => {
-    var wait = false;
 
-    return () => {
-        if (!wait) {
-            callback.call();
-            wait = true;
-
-            window.setTimeout(() => {
-                wait = false;
-            }, limit);
-        }
-    };
-};
 const supplant = function(str, o) {
     return str.replace(/\{([^{}]*)\}/g, function(a, b) {
         var r = o[b];
@@ -158,7 +102,7 @@ const formatDecimal = function(num, options) {
     }
     cents = options.precision ? cents.substring(0, options.precision + 1) : '';
     var temp = integers.join('');
-    if (options.quantile > 1) {
+    if (options.quantile > 0) {
         var remain = integers.length % options.quantile;
         temp = integers.reduce(function(previousValue, currentValue, index) {
             if (index + 1 === remain || (index + 1 - remain) % options.quantile === 0) {
@@ -176,54 +120,6 @@ const formatDecimal = function(num, options) {
     return rtn;
 };
 
-function merge( /* obj1, obj2, obj3, ... */ ) {
-    var result = {};
-
-    function assignValue(val, key) {
-        if (typeof result[key] === 'object' && typeof val === 'object') {
-            result[key] = merge(result[key], val);
-        } else {
-            result[key] = val;
-        }
-    }
-
-    for (var i = 0, l = arguments.length; i < l; i++) {
-        forEach(arguments[i], assignValue);
-    }
-    return result;
-}
-
-function extend(a, b, thisArg) {
-    forEach(b, function assignValue(val, key) {
-        if (thisArg && typeof val === 'function') {
-            a[key] = bind(val, thisArg);
-        } else {
-            a[key] = val;
-        }
-    });
-    return a;
-}
-
-function forEach(obj, fn) {
-    if (obj === null || typeof obj === 'undefined') {
-        return;
-    }
-    if (typeof obj !== 'object' && !isArray(obj)) {
-        obj = [obj];
-    }
-
-    if (isArray(obj)) {
-        for (var i = 0, l = obj.length; i < l; i++) {
-            fn.call(null, obj[i], i, obj);
-        }
-    } else {
-        for (var key in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, key)) {
-                fn.call(null, obj[key], key, obj);
-            }
-        }
-    }
-}
 const common = {
     isArray,
     uniqueId,
