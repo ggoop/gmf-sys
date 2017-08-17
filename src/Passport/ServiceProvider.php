835e217fe6e836fc2374e269d149fefdbdf31b4a
刚说 <samuel_abe@163.com>
2017-08-17 09:45:14 +0800

    init

--- src/Passport/ServiceProvider.php
+++ src/Passport/ServiceProvider.php
@@ -165,9 +165,9 @@ class ServiceProvider extends BaseServiceProvider {
 			'file://' . Passport::keyPath('oauth-private.key'),
 			'file://' . Passport::keyPath('oauth-public.key')
 		);
-		//if (!empty($server->setEncryptionKey)) {
-		$server->setEncryptionKey(app('encrypter')->getKey());
-		//}
+		if (!empty($server->setEncryptionKey)) {
+			$server->setEncryptionKey(app('encrypter')->getKey());
+		}
 
 		return $server;
 	}
