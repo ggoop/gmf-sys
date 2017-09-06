4183ded519f11e76ae525159b18503273025ed14
wuyg <samuel_abe@163.com>
2017-09-06 12:29:43 +0800

    init

--- src/Libs/Common.php
+++ src/Libs/Common.php
@@ -60,22 +60,26 @@ class Common {
 		return $rtn;
 	}
 	public static function DecryptDES($data, $key) {
-		$data = base64_decode($data);
-		$td = mcrypt_module_open(MCRYPT_DES, "", MCRYPT_MODE_ECB, ""); //使用MCRYPT_DES算法,ecb模式
-		$size = mcrypt_enc_get_iv_size($td); //设置初始向量的大小
-		$iv = mcrypt_create_iv($size, MCRYPT_RAND); //创建初始向量
-		$key_size = mcrypt_enc_get_key_size($td); //返回所支持的最大的密钥长度（以字节计算）
+		try {
+			$rtn = base64_decode($data);
+			$td = mcrypt_module_open(MCRYPT_DES, "", MCRYPT_MODE_ECB, ""); //使用MCRYPT_DES算法,ecb模式
+			$size = mcrypt_enc_get_iv_size($td); //设置初始向量的大小
+			$iv = mcrypt_create_iv($size, MCRYPT_RAND); //创建初始向量
+			$key_size = mcrypt_enc_get_key_size($td); //返回所支持的最大的密钥长度（以字节计算）
 
-		$salt = '';
-		$subkey = substr(strtoupper(md5($key)), 0, $key_size); //对key复杂处理，并设置长度
-		mcrypt_generic_init($td, $subkey, $iv);
-		$rtn = rtrim(mdecrypt_generic($td, $data));
-		mcrypt_generic_deinit($td);
-		mcrypt_module_close($td);
+			$salt = '';
+			$subkey = substr(strtoupper(md5($key)), 0, $key_size); //对key复杂处理，并设置长度
+			mcrypt_generic_init($td, $subkey, $iv);
 
-		$rtn = static::pkcs5_unpad($rtn);
+			$rtn = rtrim(mdecrypt_generic($td, $rtn));
+			mcrypt_generic_deinit($td);
+			mcrypt_module_close($td);
 
-		return $rtn;
+			$data = static::pkcs5_unpad($rtn);
+		} catch (\Exception $ex) {
+
+		}
+		return $data;
 	}
 	private static function pkcs5_pad($text, $blocksize) {
 		$pad = $blocksize - (strlen($text) % $blocksize);
