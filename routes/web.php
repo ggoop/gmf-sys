b7ba2a1139a4848edb3c80017c98f59f53aa6a34
刚说 <samuel_abe@163.com>
2017-11-01 14:25:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ Route::middleware(['web'])
 	->prefix('sys/lns')
 	->namespace('Gmf\Sys\Http\Controllers')
 	->group(function () {
-		Route::get('regist', ['uses' => 'LnsController@getWebRegist']);
-		Route::post('regist', ['uses' => 'LnsController@storeWebRegist']);
+		//Route::get('regist', ['uses' => 'LnsController@getWebRegist']);
+		//Route::post('regist', ['uses' => 'LnsController@storeWebRegist']);
 	});
