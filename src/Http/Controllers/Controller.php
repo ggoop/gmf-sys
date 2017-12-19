c70867acb28c9a42f86f872190b72489be2f357e
wuyg <samuel_abe@163.com>
2017-12-19 15:50:16 +0800

    init

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -10,13 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController {
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-	protected function toJson($result, Closure $callback = null) {
+	public function toJson($result, Closure $callback = null) {
 		return APIResult::json($result, $callback);
 	}
-	protected function errorParam($paramName, Closure $callback = null) {
+	public function errorParam($paramName, Closure $callback = null) {
 		return APIResult::errorParam($paramName);
 	}
-	protected function toError($msg, Closure $callback = null) {
+	public function toError($msg, Closure $callback = null) {
 		return APIResult::error($msg, $callback);
 	}
 }
