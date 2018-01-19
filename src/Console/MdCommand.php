e5e5a07ee3b9a6dd661a9e8dd3af126c49e401e8
pull user <pull user@163.com>
2018-01-19 18:37:02 +0800

    init

--- src/Console/MdCommand.php
+++ src/Console/MdCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use PDO;
 
 class MdCommand extends Command {
 	/**
@@ -30,12 +31,38 @@ class MdCommand extends Command {
 		parent::__construct();
 		$this->migrator = app('migrator');
 	}
+	private function getPDOConnection($host, $port, $username, $password) {
+		return new PDO(sprintf('mysql:host=%s;port=%d;', $host, $port), $username, $password);
+	}
+	private function initDatabase() {
+		$database = env('DB_DATABASE', false);
+
+		if (!$database) {
+			$this->info('Skipping creation of database as env(DB_DATABASE) is empty');
+			return;
+		}
+		try {
+
+			$pdo = $this->getPDOConnection(env('DB_HOST'), env('DB_PORT'), env('DB_USERNAME'), env('DB_PASSWORD'));
+			$pdo->exec(sprintf(
+				'CREATE DATABASE IF NOT EXISTS %s CHARACTER SET %s COLLATE %s;',
+				$database,
+				env('DB_CHARSET', 'utf8'),
+				env('DB_COLLATION', 'utf8_general_ci')
+			));
+			$this->info(sprintf('Successfully created %s database', $database));
+		} catch (PDOException $exception) {
+			$this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
+		}
+	}
 	/**
 	 * Execute the console command.
 	 *
 	 * @return void
 	 */
 	public function handle() {
+		$this->initDatabase();
+
 		if (!$this->migrator->repositoryExists()) {
 			$this->call('migrate:install');
 		}
