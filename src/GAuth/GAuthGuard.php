6a71ad0ce7322a3a58f7a0e8b3827a00bab7fa94
pull user <pull user@163.com>
2018-05-04 14:08:52 +0800

    init

--- src/GAuth/GAuthGuard.php
+++ src/GAuth/GAuthGuard.php
@@ -11,18 +11,20 @@ class GAuthGuard {
 	protected $m_client;
 
 	protected $m_forged = false;
+	protected $user_links = [];
 
 	public function SESSION_ENT_KEY() {
 		return config('gmf.ent.session');
 	}
-
 	public function setForged($forged = true) {
 		$this->m_forged = $forged;
 	}
 	public function forged() {
 		return $this->m_forged;
 	}
-
+	public function ids() {
+		return $this->user_links;
+	}
 	public function id() {
 		return $this->userId();
 	}
@@ -137,7 +139,20 @@ class GAuthGuard {
 		return '';
 	}
 	public function setUser($user) {
+		$this->user_links = [];
 		$this->m_user = $user;
+		if ($user) {
+			$links = DB::table('gmf_sys_user_links as l')->where('l.fm_user_id', $user->id)->pluck('to_user_id');
+			if ($links->count()) {
+				$links = $links->merge(DB::table('gmf_sys_user_links as l')->whereIn('l.fm_user_id', $links->all())->pluck('to_user_id')->all());
+			}
+			if ($links->count()) {
+				$this->user_links = $links->unique()->reject(function ($v) {return empty($v);})->values()->all();
+			}
+		}
+		if (!in_array($user->id, $this->user_links)) {
+			$this->user_links[] = $user->id;
+		}
 		return $this;
 	}
 }
