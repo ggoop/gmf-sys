89ce89c88b62e756efa52ce219e1c0c15f83b89d
suite <amiysd@163.com>
2018-08-26 20:19:34 +0800

    init

--- src/Http/Controllers/DataController.php
+++ src/Http/Controllers/DataController.php
@@ -3,36 +3,50 @@
 namespace Gmf\Sys\Http\Controllers;
 use DB;
 use GAuth;
-use Uuid;
+use Gmf\Sys\Builder;
+use Gmf\Sys\Libs\TreeBuilder;
 use Illuminate\Http\Request;
+use Uuid;
 
 class DataController extends Controller {
-	public function index(Request $request) {
-		$datas = [];
-		for ($i = 0; $i < 20; $i++) {
-			$datas[] = Uuid::generate();
-		}
-		return $this->toJson($datas);
-	}
-	public function show(Request $request) {
-		return GAuth::entId();
+  public function index(Request $request) {
+    $b = new TreeBuilder([
+      new Builder(['id' => 'A']),
+      new Builder(['id' => 'B', 'parent_id' => 'A']),
+      new Builder(['id' => 'C', 'parent_id' => 'B']),
+      new Builder(['id' => 'D']),
+      new Builder(['id' => 'E', 'parent_id' => 'A']),
+      new Builder(['id' => 'F']),
+      new Builder(['id' => 'G']),
+      new Builder(['id' => 'H']),
+    ]);
+    return $this->toJson($b->build());
 
-		$datas = Uuid::generate();
-		return $this->toJson($datas);
-	}
-	public function issueUid(Request $request) {
-		return $this->toJson(Uuid::generate());
-	}
-	public function issueSn(Request $request) {
-		$num = $request->input('num', 1);
-		$num = intval($num);
-		$num = $num > 0 ? $num : 1;
-		DB::statement('SET @num =' . $num . ';');
-		DB::statement("CALL sp_gmf_sys_uid(?,@num);", [$request->input('node')]);
-		$datas = DB::select('select @num as sn');
-		return $this->toJson($datas && count($datas) ? $datas[0]->sn : false);
-	}
-	public function dataImport(Request $request) {
-		return $this->toJson(app('Gmf\Sys\Bp\DataImport')->create($request));
-	}
+    $datas = [];
+    for ($i = 0; $i < 20; $i++) {
+      $datas[] = Uuid::generate();
+    }
+    return $this->toJson($datas);
+  }
+  public function show(Request $request) {
+    return GAuth::entId();
+
+    $datas = Uuid::generate();
+    return $this->toJson($datas);
+  }
+  public function issueUid(Request $request) {
+    return $this->toJson(Uuid::generate());
+  }
+  public function issueSn(Request $request) {
+    $num = $request->input('num', 1);
+    $num = intval($num);
+    $num = $num > 0 ? $num : 1;
+    DB::statement('SET @num =' . $num . ';');
+    DB::statement("CALL sp_gmf_sys_uid(?,@num);", [$request->input('node')]);
+    $datas = DB::select('select @num as sn');
+    return $this->toJson($datas && count($datas) ? $datas[0]->sn : false);
+  }
+  public function dataImport(Request $request) {
+    return $this->toJson(app('Gmf\Sys\Bp\DataImport')->create($request));
+  }
 }
--- src/Libs/TreeBuilder.php
+++ src/Libs/TreeBuilder.php
@@ -1,30 +1,49 @@
 <?php
 
 namespace Gmf\Sys\Libs;
+use Illuminate\Support\Collection;
 
 class TreeBuilder {
-	public static function create($data) {
-		$builder = [];
-		foreach ($data as $key => $value) {
-			if (empty($value->parent_id) || $value->parent_id == $value->id) {
-				static::fillChild($value, $data);
-				$builder[] = $value;
-			}
-		}
-		return $builder;
-	}
-	private static function fillChild($menu, $allMenus) {
-		$childs = [];
-		foreach ($allMenus as $key => $value) {
-			if ($menu->id == $value->parent_id && $menu->id != $value->id) {
-				static::fillChild($value, $allMenus);
-				$childs[] = $value;
-			}
-		}
-		if (count($childs) > 0) {
-			$menu->childs = $childs;
-		} else {
-			$menu->childs = [];
-		}
-	}
+  private $nodes = [];
+  public function __construct($nodes) {
+    $this->nodes = $nodes;
+  }
+  public function build() {
+    if (is_array($this->nodes)) {
+      $nodes = $this->nodes;
+    } else if ($this->nodes instanceof Collection) {
+      $nodes = $this->nodes->all();
+    } else {
+      $nodes = $this->nodes;
+    }
+    foreach ($nodes as $k => $v) {
+      if (empty($v)) {
+        continue;
+      }
+      //查询子节点
+      $subs = [];
+      foreach ($nodes as $sk => $sv) {
+        if ($sv && $sv->parent_id == $v->id && $sv->id != $v->id) {
+          $subs[] = $sv;
+          $nodes[$sk] = null;
+        }
+      }
+      //如果存在子节点
+      if (count($subs)) {
+        $v->childs = $subs;
+      }
+    }
+    $ns = [];
+    foreach ($nodes as $k => $v) {
+      if (!empty($v)) {
+        $ns[] = $v;
+      }
+    }
+    return $ns;
+  }
+
+  public static function create($data) {
+    $b = new TreeBuilder($data);
+    return $b->build();
+  }
 }
