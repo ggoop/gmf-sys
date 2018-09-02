19eca0ba466c618e6a0962032989991a98efe64c
suite <amiysd@163.com>
2018-09-02 22:45:32 +0800

    init

--- resources/assets/js/components/MdTabs/MdTabs.vue
+++ resources/assets/js/components/MdTabs/MdTabs.vue
@@ -26,15 +26,15 @@
   </div>
 </template>
 <script>
-import raf from 'raf'
-import MdComponent from 'gmf/core/MdComponent'
-import MdAssetIcon from 'gmf/core/mixins/MdAssetIcon/MdAssetIcon'
-import MdPropValidator from 'gmf/core/utils/MdPropValidator'
-import MdObserveElement from 'gmf/core/utils/MdObserveElement'
-import MdContent from 'gmf/components/MdContent/MdContent'
+import raf from "raf";
+import MdComponent from "gmf/core/MdComponent";
+import MdAssetIcon from "gmf/core/mixins/MdAssetIcon/MdAssetIcon";
+import MdPropValidator from "gmf/core/utils/MdPropValidator";
+import MdObserveElement from "gmf/core/utils/MdObserveElement";
+import MdContent from "gmf/components/MdContent/MdContent";
 
 export default new MdComponent({
-  name: 'MdTabs',
+  name: "MdTabs",
   mixins: [MdAssetIcon],
   components: {
     MdContent
@@ -42,8 +42,8 @@ export default new MdComponent({
   props: {
     mdAlignment: {
       type: String,
-      default: 'left',
-      ...MdPropValidator('md-alignment', ['left', 'right', 'centered', 'fixed'])
+      default: "left",
+      ...MdPropValidator("md-alignment", ["left", "right", "centered", "fixed"])
     },
     mdElevation: {
       type: [Number, String],
@@ -72,201 +72,221 @@ export default new MdComponent({
   provide() {
     return {
       MdTabs: this.MdTabs
-    }
+    };
   },
   computed: {
     tabsClasses() {
       return {
-        ['md-alignment-' + this.mdAlignment]: true,
-        'md-no-transition': this.noTransition,
-        'md-dynamic-height': this.mdDynamicHeight
-      }
+        ["md-alignment-" + this.mdAlignment]: true,
+        "md-no-transition": this.noTransition,
+        "md-dynamic-height": this.mdDynamicHeight
+      };
     },
     contentClass() {
       return {
-        'md-transparent': this.mdContentTransparent
-      }
+        "md-transparent": this.mdContentTransparent
+      };
     },
     navigationClasses() {
-      return 'md-elevation-' + this.mdElevation
-    },
+      return "md-elevation-" + this.mdElevation;
+    }
   },
   watch: {
     MdTabs: {
       deep: true,
       handler() {
-        this.setHasContent()
+        this.setHasContent();
       }
     },
     activeTab() {
       this.$nextTick().then(() => {
-        this.setIndicatorStyles()
-        this.setActiveTabIndex()
-        this.calculateTabPos()
-      })
+        this.setIndicatorStyles();
+        this.setActiveTabIndex();
+        this.calculateTabPos();
+      });
     },
     mdActiveTab(tab) {
-      this.activeTab = tab
-      this.$emit('md-changed', tab)
+      this.activeTab = tab;
+      this.$emit("md-changed", tab);
     }
   },
   methods: {
     hasActiveTab() {
-      return this.activeTab || this.mdActiveTab
+      return this.activeTab || this.mdActiveTab;
     },
     getItemsAndKeys() {
-      const items = this.MdTabs.items
+      const items = this.MdTabs.items;
 
       return {
         items,
         keys: Object.keys(items)
-      }
+      };
     },
     setActiveTab(index) {
-      this.activeTab = index
-      this.$emit('md-changed', index)
+      this.activeTab = index;
+      this.$emit("md-changed", index);
     },
     setActiveTabIndex() {
-      const activeButton = this.$el.querySelector('.md-button.md-active')
+      const activeButton = this.$el.querySelector(".md-button.md-active");
 
       if (activeButton) {
-        this.activeTabIndex = [].indexOf.call(activeButton.parentNode.childNodes, activeButton)
+        this.activeTabIndex = [].indexOf.call(
+          activeButton.parentNode.childNodes,
+          activeButton
+        );
       }
     },
     setActiveTabByIndex(index) {
-      const { keys } = this.getItemsAndKeys()
+      const { keys } = this.getItemsAndKeys();
 
       if (!this.hasActiveTab()) {
-        this.activeTab = keys[index]
+        this.activeTab = keys[index];
       }
     },
     setActiveTabByRoute() {
-      const { items, keys } = this.getItemsAndKeys()
-      let tabIndex = null
+      const { items, keys } = this.getItemsAndKeys();
+      let tabIndex = null;
 
       if (this.$router) {
         keys.forEach((key, index) => {
-          const item = items[key]
-          const toProp = item.props.to
+          const item = items[key];
+          const toProp = item.props.to;
 
           if (toProp && toProp === this.$route.path) {
-            tabIndex = index
+            tabIndex = index;
           }
-        })
+        });
       }
 
       if (!this.hasActiveTab() && !tabIndex) {
-        this.activeTab = keys[0]
+        this.activeTab = keys[0];
       } else {
-        this.activeTab = keys[tabIndex]
+        this.activeTab = keys[tabIndex];
       }
     },
     setHasContent() {
-      const { items, keys } = this.getItemsAndKeys()
+      const { items, keys } = this.getItemsAndKeys();
 
-      this.hasContent = keys.some(key => items[key].hasContent)
+      this.hasContent = keys.some(key => items[key].hasContent);
     },
     setIndicatorStyles() {
       raf(() => {
         this.$nextTick().then(() => {
-          const activeButton = this.$el.querySelector('.md-button.md-active')
+          const activeButton = this.$el.querySelector(".md-button.md-active");
 
           if (activeButton && this.$refs.indicator) {
-            const buttonWidth = activeButton.offsetWidth
-            const buttonLeft = activeButton.offsetLeft
-            const indicatorLeft = this.$refs.indicator.offsetLeft
+            const buttonWidth = activeButton.offsetWidth;
+            const buttonLeft = activeButton.offsetLeft;
+            const indicatorLeft = this.$refs.indicator.offsetLeft;
 
             if (indicatorLeft < buttonLeft) {
-              this.indicatorClass = 'md-tabs-indicator-right'
+              this.indicatorClass = "md-tabs-indicator-right";
             } else {
-              this.indicatorClass = 'md-tabs-indicator-left'
+              this.indicatorClass = "md-tabs-indicator-left";
             }
 
             this.indicatorStyles = {
               left: `${buttonLeft}px`,
               width: `${buttonWidth}px`
-            }
+            };
           }
-        })
-      })
+        });
+      });
     },
     calculateTabPos() {
       if (this.hasContent) {
-        const tabElement = this.$el.querySelector(`.md-tab:nth-child(${this.activeTabIndex + 1})`)
-        this.contentStyles = {
-          height: `${tabElement.offsetHeight}px`
+        const tabElement = this.$el.querySelector(
+          `.md-tab:nth-child(${this.activeTabIndex + 1})`
+        );
+        if (this.mdDynamicHeight) {
+          this.contentStyles = {
+            height: `${tabElement.offsetHeight}px`
+          };
         }
         this.containerStyles = {
           transform: `translate3D(${-this.activeTabIndex * 100}%, 0, 0)`
-        }
+        };
       }
     },
     setupObservers() {
-      if ('ResizeObserver' in window) {
-        this.resizeObserver = new window.ResizeObserver(this.setIndicatorStyles)
-        this.resizeObserver.observe(this.$el)
+      if ("ResizeObserver" in window) {
+        this.resizeObserver = new window.ResizeObserver(
+          this.setIndicatorStyles
+        );
+        this.resizeObserver.observe(this.$el);
       } else {
-        this.resizeObserver = MdObserveElement(this.$el.querySelector('.md-tabs-content'), {
-          childList: true,
-          characterData: true,
-          subtree: true
-        }, () => {
-          this.setIndicatorStyles()
-          this.calculateTabPos()
-        })
+        this.resizeObserver = MdObserveElement(
+          this.$el.querySelector(".md-tabs-content"),
+          {
+            childList: true,
+            characterData: true,
+            subtree: true
+          },
+          () => {
+            this.setIndicatorStyles();
+            this.calculateTabPos();
+          }
+        );
 
-        window.addEventListener('resize', this.setIndicatorStyles)
+        window.addEventListener("resize", this.setIndicatorStyles);
       }
     },
     setupWatchers() {
       if (this.mdSyncRoute) {
-        this.$watch('$route', {
+        this.$watch("$route", {
           deep: true,
           handler() {
             if (this.mdSyncRoute) {
-              this.setActiveTabByRoute()
+              this.setActiveTabByRoute();
             }
           }
-        })
+        });
       }
     }
   },
   created() {
-    this.setHasContent()
-    this.activeTab = this.mdActiveTab
+    this.setHasContent();
+    this.activeTab = this.mdActiveTab;
   },
   mounted() {
-    this.$nextTick().then(() => {
-      if (this.mdSyncRoute) {
-        this.setActiveTabByRoute()
-      } else {
-        this.setActiveTabByIndex(0)
-      }
+    this.$nextTick()
+      .then(() => {
+        if (this.mdSyncRoute) {
+          this.setActiveTabByRoute();
+        } else {
+          this.setActiveTabByIndex(0);
+        }
 
-      return this.$nextTick()
-    }).then(() => {
-      this.setActiveTabIndex()
-      this.calculateTabPos()
+        return this.$nextTick();
+      })
+      .then(() => {
+        this.setActiveTabIndex();
+        this.calculateTabPos();
 
-      window.setTimeout(() => {
-        this.noTransition = false
-        this.setupObservers()
-        this.setupWatchers()
-      }, 100)
-    })
+        window.setTimeout(() => {
+          this.noTransition = false;
+          this.setupObservers();
+          this.setupWatchers();
+        }, 100);
+      });
 
-    this.$refs.navigation.addEventListener('transitionend', this.setIndicatorStyles)
+    this.$refs.navigation.addEventListener(
+      "transitionend",
+      this.setIndicatorStyles
+    );
   },
   beforeDestroy() {
     if (this.resizeObserver) {
-      this.resizeObserver.disconnect()
+      this.resizeObserver.disconnect();
     }
 
-    window.removeEventListener('resize', this.setIndicatorStyles)
-    this.$refs.navigation.removeEventListener('transitionend', this.setIndicatorStyles)
+    window.removeEventListener("resize", this.setIndicatorStyles);
+    this.$refs.navigation.removeEventListener(
+      "transitionend",
+      this.setIndicatorStyles
+    );
   }
-})
-
+});
 </script>
 <style lang="scss">
 @import "~gmf/components/MdAnimation/variables";
@@ -282,7 +302,7 @@ export default new MdComponent({
   }
 
   &.md-dynamic-height .md-tabs-content {
-    transition: height .3s $md-transition-default-timing;
+    transition: height 0.3s $md-transition-default-timing;
     will-change: height;
   }
 
@@ -291,7 +311,7 @@ export default new MdComponent({
   }
 
   &.md-dynamic-height .md-tabs-content {
-    transition: height .35s $md-transition-stand-timing;
+    transition: height 0.35s $md-transition-stand-timing;
   }
 
   &.md-alignment-left .md-tabs-navigation {
@@ -333,7 +353,7 @@ export default new MdComponent({
 .md-tabs-navigation {
   display: flex;
   position: relative;
-
+  flex: none;
   .md-button {
     max-width: 564px;
     min-width: 62px;
@@ -357,7 +377,7 @@ export default new MdComponent({
       justify-content: center;
     }
 
-    .md-tab-icon+.md-tab-label {
+    .md-tab-icon + .md-tab-label {
       margin-top: 10px;
     }
   }
@@ -380,13 +400,13 @@ export default new MdComponent({
   will-change: left, right;
 
   &.md-tabs-indicator-left {
-    transition: left .3s $md-transition-default-timing,
-    right .35s $md-transition-default-timing;
+    transition: left 0.3s $md-transition-default-timing,
+      right 0.35s $md-transition-default-timing;
   }
 
   &.md-tabs-indicator-right {
-    transition: right .3s $md-transition-default-timing,
-    left .35s $md-transition-default-timing;
+    transition: right 0.3s $md-transition-default-timing,
+      left 0.35s $md-transition-default-timing;
   }
 }
 
@@ -396,7 +416,7 @@ export default new MdComponent({
   will-change: height;
 
   &.md-transparent {
-    background-color: transparent!important;
+    background-color: transparent !important;
   }
 }
 
@@ -405,7 +425,7 @@ export default new MdComponent({
   align-items: flex-start;
   flex-wrap: nowrap;
   transform: translateZ(0);
-  transition: transform .35s $md-transition-default-timing;
+  transition: transform 0.35s $md-transition-default-timing;
   will-change: transform;
 }
 
@@ -418,5 +438,4 @@ export default new MdComponent({
     padding: 8px;
   }
 }
-
 </style>
