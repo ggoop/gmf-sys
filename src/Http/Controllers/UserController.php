2c112691d817672c411cf18a841091668cabff0c
gop <>
2018-09-10 10:34:56 +0800

    init

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -1,18 +1,24 @@
 <?php
 
 namespace Gmf\Sys\Http\Controllers;
+
 use GAuth;
 use Illuminate\Http\Request;
 
-class UserController extends Controller {
-  public function index(Request $request) {
+class UserController extends Controller
+{
+  public function index(Request $request)
+  {
     $query = config('gmf.user.model')::where('id', '!=', '')->whereIn('id', GAuth::ids());
     $data = $query->get();
     return $this->toJson($data);
   }
-  public function show(Request $request, string $id) {
-    $query = config('gmf.user.model')::where('id', '!=', '');
-    $data = $query->where('id', $id)->first();
+  public function show(Request $request, string $id)
+  {
+    $query = config('gmf.user.model')::whereNotNull('account')->whereNotNull('openid');
+    $data = $query->where(function ($query) use ($id) {
+      $query->where('id', $id)->orWhere('openid', $id);
+    })->first();
     return $this->toJson($data);
   }
 }
