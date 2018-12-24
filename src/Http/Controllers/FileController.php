b7986d34de1a753d45a27a77ec8331d2989a5a75
吴运刚 <wuyg@yonyou.com>
2018-12-24 14:28:32 +0800

    add check mes

--- src/Http/Controllers/FileController.php
+++ src/Http/Controllers/FileController.php
@@ -5,6 +5,7 @@ use GAuth;
 use Gmf\Sys\Http\Resources;
 use Gmf\Sys\Models\File;
 use Illuminate\Http\Request;
+use Validator;
 
 class FileController extends Controller
 {
@@ -19,9 +20,12 @@ class FileController extends Controller
   {
     GAuth::check('user');
     $fname = $request->input('name', 'files');
-    $validatedData = $request->validate([
-      $fname =>'mimes:jpeg,gif,bmp,png,docx,doc,txt,xlsx,ppt,pptx,pdf'
-      ]);
+    $validator = Validator::make($request->all(), [
+      $fname => 'mimes:jpeg,gif,bmp,png,docx,doc,txt,xlsx,ppt,pptx,pdf'
+    ]);
+    if ($validator->fails()) {
+      throw new \Exception('嘿，是要进行攻击吗~');
+    }
     $files = File::storage($request, $request->input('name', 'files'), $request->input('path', 'file'));
     if ($files) {
       return $this->toJson(Resources\File::collection($files));
